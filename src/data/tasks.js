3988341ead3ab66be68ef9c66ba0730a5b1a9398
niadul-munna <niadulislammunna@gmail.com>
2025-06-09 23:38:07 +0600

    initial data

---
+++ src/data/tasks.js
@@ -0,0 +1,6 @@
+const initialTasks = [
+  { id: 0, text: "Learn React", done: true },
+  { id: 1, text: "Learn Redux", done: false },
+  { id: 2, text: "Learn Next.js", done: false },
+];
+export default initialTasks;
